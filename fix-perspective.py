2b9b15880bd3f17426a07424da9d8372538ff3ff
mikel.sanchez <mikel.sanchez@eigentech.com>
2020-10-16 14:27:13 +0100

    Add comment

--- fix-perspective.py
+++ fix-perspective.py
@@ -46,5 +46,6 @@ if args:
     # view of the original image
     warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
 
+    # Save the image in the fixed directory
     Image.fromarray(np.uint8(warped)).save('fixed/' + args["image"])
 
